d118dd3fe3b4e12c761d4f2105f9dad21523e173
Biraru <muhammadbilalalisyed@gmail.com>
2024-12-25 23:16:58 +0500

    Fixed Lineage Error

--- lineage_emerald.mk
+++ lineage_emerald.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from generic device
 $(call inherit-product, device/xiaomi/emerald/device.mk)
